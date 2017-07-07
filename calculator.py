dd9af2ed07b33ab2b94ef24fe6cfd480d1a29a1c
Megan <megan.e.dose@gmail.com>
2017-07-06 17:06:30 -0700

    Working Calculator. Added try exception for IndexError.

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 """A prefix-notation calculator.
 
-Using the arithmetic.py file from Calculator Part 1, create the
+Using the py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
@@ -9,11 +9,35 @@ from arithmetic import *
 
 # Your code goes here
 while True:
-    user_input = raw_input("> ")
-    token = user_input.split(" ")
-    if token[0] == "q":
-        break
-    else:
-        if user_input[0] == "+":
-            print arithmetic.add(user_input[1:])
-
+    try:
+        user_input = raw_input("> ")
+        token = user_input.split(" ")
+        #int_tokens = int(token[1])
+        if token[0] == "q":
+            break
+        elif token[0] == "+":
+            print add(int(token[1]), int(token[2]))
+        elif token[0] == "-":
+            print subtract(int(token[1]), int(token[2]))
+        elif token[0] == "*":
+            print multiply(int(token[1]), int(token[2]))
+        elif token[0] == "*":
+            print multiply(int(token[1]), int(token[2]))
+        elif token[0] == "/":
+            print divide(int(token[1]), int(token[2]))
+        elif token[0] == "square":
+            print square(int(token[1]))
+        elif token[0] == "cube":
+            print cube(int(token[1]))
+        elif token[0] == "pow":
+            print power(int(token[1]), int(token[2]))
+        elif token[0] == "mod":
+            print mod(int(token[1]), int(token[2]))
+        elif token[0] == "x+":
+            print add_mult(int(token[1]), int(token[2]), int(token[3]))
+        elif token[0] == "cubes+":
+            print add_cubes(int(token[1]), int(token[2]))
+        else:
+            print "That's not a valid entry. Please enter an operand and intergers."
+    except IndexError:
+        print "That's not a valid entry. Please enter an operand and intergers."
