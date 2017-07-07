0275e4cbbf9a73ab95aef867242e255a1c72043d
Megan <megan.e.dose@gmail.com>
2017-07-06 17:25:52 -0700

    Clean up code

--- calculator.py
+++ calculator.py
@@ -12,31 +12,35 @@ while True:
     try:
         user_input = raw_input("> ")
         token = user_input.split(" ")
-        #int_tokens = int(token[1])
+        num1 = int(token[1])
+        if len(token) > 2:
+            num2 = int(token[2])
+        if len(token) > 3:
+            num3 = int(token[3])
         if token[0] == "q":
             break
         elif token[0] == "+":
-            print add(int(token[1]), int(token[2]))
+            print add(num1, num2)
         elif token[0] == "-":
-            print subtract(int(token[1]), int(token[2]))
+            print subtract(num1, num2)
         elif token[0] == "*":
-            print multiply(int(token[1]), int(token[2]))
+            print multiply(num1, num2)
         elif token[0] == "*":
-            print multiply(int(token[1]), int(token[2]))
+            print multiply(num1, num2)
         elif token[0] == "/":
-            print divide(int(token[1]), int(token[2]))
+            print divide(num1, num2)
         elif token[0] == "square":
-            print square(int(token[1]))
+            print square(num1)
         elif token[0] == "cube":
-            print cube(int(token[1]))
+            print cube(num1)
         elif token[0] == "pow":
-            print power(int(token[1]), int(token[2]))
+            print power(num1, num2)
         elif token[0] == "mod":
-            print mod(int(token[1]), int(token[2]))
+            print mod(num1, num2)
         elif token[0] == "x+":
-            print add_mult(int(token[1]), int(token[2]), int(token[3]))
+            print add_mult(num1, num2, num3)
         elif token[0] == "cubes+":
-            print add_cubes(int(token[1]), int(token[2]))
+            print add_cubes(num1, num2)
         else:
             print "That's not a valid entry. Please enter an operand and intergers."
     except IndexError:
